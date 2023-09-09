65759b4657aa43725b8a2be53b5c9ece5388e7b0
felperaa <galvao.felipe23@gmail.com>
2023-09-09 16:12:11 -0300

    Implemented ADALINE with iris dataset

--- adaline.py
+++ adaline.py
@@ -2,9 +2,9 @@ import numpy as np
 import matplotlib.pyplot as plt
 from random import shuffle
 from sklearn import datasets
+from sklearn.preprocessing import StandardScaler
 
-
-def ADALINE_Online(X, W, tol_error, max_epochs, bias):
+def ADALINE_Online(X, W, tol_error, max_epochs, bias, learning_rate):
     epochs = 0
     errors = []
 
@@ -14,14 +14,11 @@ def ADALINE_Online(X, W, tol_error, max_epochs, bias):
         shuffle(indices)
 
         for i in indices:
-            # Calcular o sinal do neurônio (spike)
-            V = np.dot(W, X[i]) + bias
-            # Ativação Linear
+            V = np.dot(W, X[i]) + (bias * W[-1])
             Y = V
             error += (D[i] - Y) ** 2
-            # Atualizar os pesos sinápticos da rede para todos os exemplos (SEMPRE)
-            W = W + (D[i] - Y) * X[i]
-        # Calcular o erro quadrático médio da época (MSE) usando todas as instâncias
+            W = W + learning_rate * (D[i] - Y) * X[i]
+
         mse = (1 / X.shape[0]) * error
         errors.append(mse)
         epochs += 1
@@ -30,23 +27,25 @@ def ADALINE_Online(X, W, tol_error, max_epochs, bias):
 
     return W, errors
 
-
 iris = datasets.load_iris()
 X = iris.data[:, :2]
-
 D = iris.target
 
-X_mean = X.mean(axis=0)
-X_std = X.std(axis=0)
-X = (X - X_mean) / (X_std + 1e-8)
+# Normalização das características usando StandardScaler
+scaler = StandardScaler()
+X = scaler.fit_transform(X)
+
 X = np.hstack((X, np.ones((X.shape[0], 1))))
 
-W = np.random.rand(X.shape[1])
-bias = 1.0
-tol_error = 0.001
-max_epochs = 1000
+# Inicialização dos pesos mais próximos de zero
+W = np.array([0.01, 0.02, 0.03])
 
-final_weights, errors = ADALINE_Online(X, W, tol_error, max_epochs, bias)
+bias = 1.0
+tol_error = 0.0001
+max_epochs = 100  # Alterando max_epochs para 100
+learning_rate = 0.0001  # Alterando learning_rate para 0.0001
+
+final_weights, errors = ADALINE_Online(X, W, tol_error, max_epochs, bias, learning_rate)
 
 plt.plot(range(len(errors)), errors)
 plt.xlabel('Épocas')
